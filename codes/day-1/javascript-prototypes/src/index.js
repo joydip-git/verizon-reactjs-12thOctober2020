dcd77d85b82d96031398e03f7768822f214a014a
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-12 15:15:58 +0530

    Update index.js

--- codes/day-1/javascript-prototypes/src/index.js
+++ codes/day-1/javascript-prototypes/src/index.js
@@ -1,34 +1,49 @@
-function employee(name, id, basicPay, daPay, hraPay) {
+//there is a prototype object for employee function
+//employee.prototype
+//employee.prototype.__proto__ = Object.prototype
+var employee = function (name, id, basicPay, daPay, hraPay) {
     this.name = name;
     this.id = id;
     this.basicPay = basicPay;
     this.daPay = daPay;
     this.hraPay = hraPay;
-    this.calculateSalary = function () {
-        return this.basicPay + this.daPay + this.hraPay;
-    }
+}
+employee.prototype.calculateSalary = function () {
+    return this.basicPay + this.daPay + this.hraPay;
 }
 
+//there is a prototype object for developer function
+//developer.prototype
+//developer.prototype.__proto__ = Object.prototype
 function developer(name, id, basicPay, daPay, hraPay, incentivePay) {
-    this.name = name;
-    this.id = id;
-    this.basicPay = basicPay;
-    this.daPay = daPay;
-    this.hraPay = hraPay;
+    employee.call(this, name, id, basicPay, daPay, hraPay);
     this.incentivePay = incentivePay;
     this.calculateSalary = function () {
-        return this.basicPay + this.daPay + this.hraPay + this.incentivePay;
+        var salary = employee.prototype.calculateSalary.apply(this);
+        return salary + this.incentivePay;
     }
 }
 
+var anilDev = new developer('anil', 'gupta', 1000, 2000, 3000, 4000);
+
+//there is a prototype object for hr function
+//hr.prototype
+//hr.prototype.__proto__ = Object.prototype
 function hr(name, id, basicPay, daPay, hraPay, gratuityPay) {
-    this.name = name;
-    this.id = id;
-    this.basicPay = basicPay;
-    this.daPay = daPay;
-    this.hraPay = hraPay;
+    employee.call(this, name, id, basicPay, daPay, hraPay);
     this.gratuityPay = gratuityPay;
     this.calculateSalary = function () {
-        return this.basicPay + this.daPay + this.hraPay + this.gratuityPay;
+        var salary = employee.prototype.calculateSalary.apply(this);
+        return salary + this.gratuityPay;
     }
 }
+
+var sunilHr = new hr('sunil', 'kumar', 2000, 3000, 4000, 5000);
+
+var anilSalary = anilDev.calculateSalary();
+var sunilSalary = sunilHr.calculateSalary();
+
+console.log(anilSalary);
+console.log(sunilSalary)
+
+console.log(window)
