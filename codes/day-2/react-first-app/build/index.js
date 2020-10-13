22aed89d32836b24c778fb7d11c774ff9fffea69
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-13 13:34:21 +0530

    day-2 prelunch update

--- codes/day-2/react-first-app/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var appElement = (0, _app["default"])();
-var appDivElement = document.getElementById('app');
-appDivElement.appendChild(appElement);
---
+++ codes/day-2/react-first-app/webpack.config.js
@@ -0,0 +1,10 @@
+//1. input files: 
+//entry file: index.js
+//2. output:
+//build, bundles.js
+//3. use loaders
+//4. source map
+//5. dev server
+//6. plugins:
+
+
