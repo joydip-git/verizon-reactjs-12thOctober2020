fa649819a7eebebd3ab71ab787b49b0cca9378de
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-14 11:21:05 +0530

    app updated

--- codes/day-3/react-state-list-demo/src/App.js
+++ codes/day-3/react-state-list-demo/src/App.js
@@ -23,16 +23,44 @@ class App extends Component {
         }
       ]
     }
+    console.log('[App] ctor called')
   }
+  personChangeHandler = (propertyName, newPropertyValue, personId) => {
+    const foundPersonRef = this.state.people.find((p) => p.id === personId);
+    const foundPersonIndex = this.state.people.findIndex((p) => p.id === personId);
 
+    let copyOfPeople = [...this.state.people]
+    let copyOfFoundReson = { ...foundPersonRef }; //{ id: 1, name: 'anil', age: 20}
+
+    copyOfFoundReson[propertyName] = newPropertyValue;
+    copyOfPeople[foundPersonIndex] = copyOfFoundReson;
+
+    this.setState({
+      people: copyOfPeople
+    }, () => console.log(this.state.people));
+  }
   render() {
+    console.log('[App] rendered')
     return (
       <div className='container'>
         <div className='panel panel-primary'>
-          <Person personData={this.state.people[0]} />
+          {
+
+            this.state.people.map((p) => {
+              return <Person key={p.id} personData={p} changeHandler={this.personChangeHandler} />
+            })
+          }
         </div>
       </div>
     );
   }
 }
 export default App;
+/**
+ * {
+ *  type:'Person',
+ *  key:null,
+ *  ref:null,
+ *  props:{}
+ * }
+ */
--- codes/day-3/react-state-list-demo/src/Person.js
+++ codes/day-3/react-state-list-demo/src/Person.js
@@ -6,26 +6,30 @@ import './Person.css'
  *        id: 1,
           name: 'anil',
           age: 20
-       }
+       },
+       changeHandler={this.personChangeHandler}
  *  }
  */
 const Person = (props) => {
-    const { personData } = props;
+    console.log('[Person] rendered')
+    const { personData, changeHandler } = props;
+
     return (
         // <React.Fragment>
         <>
+            <br />
             <div className='panel panel-heading'>
-                PersonDetail
+                Details of <b>{personData.name}</b>
             </div>
             <div className='panel panel-body'>
                 Id:&nbsp;
                 <input type='text' value={personData.id} readOnly />
                 <br />
                 Name:&nbsp;
-                <input type='text' value={personData.name} />
+                <input type='text' value={personData.name} onChange={(e) => changeHandler('name', e.target.value, personData.id)} />
                 <br />
                 Age:&nbsp;
-                <input type='text' value={personData.age} />
+                <input type='text' value={personData.age} onChange={(e) => changeHandler('age', parseInt(e.target.value), personData.id)} />
             </div>
         </>
         /* </React.Fragment> */
