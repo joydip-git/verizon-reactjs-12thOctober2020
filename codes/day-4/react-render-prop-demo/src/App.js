f2fa32292e97c46b4feb4f539ca943a7c75a674a
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-15 13:56:12 +0530

    rrnder prop final update

--- codes/day-4/react-render-prop-demo/src/App.js
+++ codes/day-4/react-render-prop-demo/src/App.js
@@ -6,23 +6,35 @@ import HoverCounter from './HoverCounter';
 
 function App() {
 
-  const renderClickCounter = (counterValue, counterHandler) => {
-    return (
-      <ClickCounter counter={counterValue} changeCounter={counterHandler} name='Joydip' />
-    );
-  };
+  // const renderClickCounter = (counterValue, counterHandler) => {
+  //   return (
+  //     <ClickCounter counter={counterValue} changeCounter={counterHandler} name='Joydip' />
+  //   );
+  // };
 
-  const renderHoverCounter = (counterValue, counterHandler) => {
-    return (
-      <HoverCounter counterData={counterValue} changeCounterData={counterHandler} data={200} />
-    );
-  };
+  // const renderHoverCounter = (counterValue, counterHandler) => {
+  //   return (
+  //     <HoverCounter counterData={counterValue} changeCounterData={counterHandler} data={200} />
+  //   );
+  // };
   return (
     <div className="App">
-      <Counter render={renderClickCounter} />
+      {/* <Counter render={renderClickCounter} />
       <br />
       <br />
-      <Counter render={renderHoverCounter} />
+      <Counter render={renderHoverCounter} /> */}
+      <Counter render={
+        (counterValue, counterValueHandler) => {
+          return <ClickCounter counter={counterValue} changeCounter={counterValueHandler} name={'joydip'} />
+        }
+      } />
+      <br />
+      <br />
+      <Counter render={
+        (counterValue, counterValueHandler) => {
+          return <HoverCounter counterData={counterValue} changeCounterData={counterValueHandler} data={200} />
+        }
+      } />
     </div>
   );
 }
